098ce3eea1f2cbd08f24eee9c4cc5aac5b34fcb4
ekizashvilit <tekizashvili@gmail.com>
2023-07-11 22:58:35 +0400

    landing page structure

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,6 +2,28 @@ import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
+            illo. Dolorem incidunt dicta quo dignissimos voluptas deserunt
+            officia possimus fuga ut. Natus, quidem. Ullam voluptas autem non
+            saepe fugiat minima nobis voluptate, perspiciatis eius ipsam
+            repudiandae, ratione asperiores rerum quaerat!
+          </p>
+          <button className="btn btn-hero">Login / Register</button>
+        </div>
+      </div>
+      <img src={main} alt="job hunt" className="img main-img" />
+    </main>
+  );
 };
 export default Landing;
